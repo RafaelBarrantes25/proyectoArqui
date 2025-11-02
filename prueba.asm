008f72cdc75ee8705c53e79f688a1b1f54cb40a1
Rafael <jorabaqu2580@gmail.com>
2025-11-01 20:40:49 -0600

    avances de abrir archivo

--- prueba.asm
+++ prueba.asm
@@ -1,9 +1,18 @@
 %include "io.mac"
 
 .DATA
-texto       db      "hola",0
+        inicio1             db      "Bienvenido a Quién Quiere Ser Millonario",0
+        nombreDelArchivo    db      "preguntasDelProyecto",0
+
+.UDATA
+        buffer              resb     2048      ;Reserva para la lectura
 
 .CODE
 .STARTUP
-        PutStr    texto
+        mov     eax       5                    ;Es el código de abrir el archivo
+        mov     ebx       nombreDelArchivo     ;Para ver cuál archivo va a abrir
+        mov     ecx       0                    ;Read only
+        mov     edx       0                    ;Permisos dan igual
+        int     0x80                           ;Hace un interrupt
+        mov     ebp,eax                        ;Mueve al ebp el file descriptor para guardarlo
 .EXIT
